fd9c7a52cff01338ecf02811b4b2addc2e56bec6
Thales <thalesroberto.f@hotmail.com>
2025-01-17 00:28:24 -0300

    Microsoft IdentityModel Tokens

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -23,11 +23,53 @@ public class UsuariosController : ControllerBase
             return Ok(_usuarioRepository.Listar());
         }
         // post -> /api/usuarios
-        [HttpPost]
-        public IActionResult Cadastrar(Usuario usuario)
+        // [HttpPost]
+        // public IActionResult Cadastrar(Usuario usuario)
+        // {
+        //     _usuarioRepository.Cadastrar(usuario);
+        //     return StatusCode(201);
+        // }
+        public IActionResult Post(Usuario usuario)
         {
-            _usuarioRepository.Cadastrar(usuario);
-            return StatusCode(201);
+            Usuario usuarioBuscado = _usuarioRepository.Login(usuario.Email,
+            usuario.Senha);
+            if (usuarioBuscado == null)
+                {
+                    return NotFound("E-mail ou senha inválidos!");
+                }
+            // Se o usuário for encontrado, segue a criação do token.
+
+            // Define os dados que serão fornecidos no token - Payload.
+            var claims = new[]
+                {
+                    // Armazena na claim o e-mail usuário autenticado.
+                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+
+                    // Armazena na claim o id do usuário autenticado.
+                    new Claim(JwtRegisteredClaimNames.Jti,
+                    usuarioBuscado.Id.ToString()),
+                };
+
+            // Define a chave de acesso ao token.
+            var key = new
+                SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-
+                autenticacao"));
+
+                // Define as credenciais do token.
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                // Gera o token.
+                var token = new JwtSecurityToken(
+                    issuer: "exoapi.webapi", // Emissor do token.
+                    audience: "exoapi.webapi", // Destinatário do token.
+                    claims: claims, // Dados definidos acima.
+                    expires: DateTime.Now.AddMinutes(30), // Tempo de expiração.
+                    signingCredentials: creds // Credenciais do token.
+                );
+            // Retorna ok com o token.
+            return Ok(
+                new { token = new JwtSecurityTokenHandler().WriteToken(token) }
+            );
         }
         // get -> /api/usuarios/{id}
         [HttpGet("{id}")] // Faz a busca pelo ID.
@@ -41,14 +83,18 @@ public class UsuariosController : ControllerBase
             return Ok(usuario);
         }
         // put -> /api/usuarios/{id}
+        
         // Atualiza.
+        [Authorize]
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id, Usuario usuario)
         {
             _usuarioRepository.Atualizar(id, usuario);
             return StatusCode(204);
         }
+
         // delete -> /api/usuarios/{id}
+        [Authorize]
         [HttpDelete("{id}")]
         public IActionResult Deletar(int id)
         {
--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf667e1648a2207d998cadb17d489cad2a188e57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee105e50a486c56e3053f9afff3610d3f8f249d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
